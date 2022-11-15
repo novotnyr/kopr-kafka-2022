a27b4146a5e4787c713aa08d90aa6aae9ed78656
Robert Novotny <novotnr0@gmail.com>
2022-11-15 11:12:55 +0100

    Pridane pocitadlo pre ratanie vyrobenych aut
    Pocitadlo sa pouzije na sledovanie distribucie medzi konzumentov

--- producer/src/main/java/sk/upjs/ics/kopr/kafka/CarStringProducer.java
+++ producer/src/main/java/sk/upjs/ics/kopr/kafka/CarStringProducer.java
@@ -5,6 +5,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 
@@ -12,12 +13,14 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 public class CarStringProducer {
     private KafkaTemplate<String, String> kafka;
 
+    private AtomicInteger counter = new AtomicInteger();
+
     public CarStringProducer(KafkaTemplate<String, String> kafka) {
         this.kafka = kafka;
     }
 
     @Scheduled(fixedDelay = 2, timeUnit = SECONDS)
     public void publishCar() {
-        this.kafka.send("car", "New car produced at " + Instant.now());
+        this.kafka.send("car", "New car #" + counter.getAndIncrement() + " produced at " + Instant.now());
     }
 }
